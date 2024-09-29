7f72c81b9589e15680b6db32eaa426e7bfc2d833
Amit Kandar <kandaramit2001@gmail.com>
2024-09-29 22:13:42 +0530

    Removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRouter from './router';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
-import Committee from './pages/commitee/Commitee';
 
 function App() {
-  const [showButton, setShowButton] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      const offset = 100;
-      setShowButton(scrollPosition > offset);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-  };
   return (
     <Router>
       <Navbar />
@@ -32,13 +12,6 @@ function App() {
         <AppRouter />
       </main>
       <Footer />
-      
-      <div
-        className={`fixed hidden lg:flex bottom-4 z-50 right-4 w-14 h-14 rounded-md bg-blue-500 text-white items-center justify-center cursor-pointer ${showButton ? 'visible' : 'invisible'}`}
-        onClick={scrollToTop}
-      >
-        <i className='bx bx-up-arrow-alt text-4xl'></i>
-      </div>
     </Router>
   );
 }
